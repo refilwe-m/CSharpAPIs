2c44298a155c14280330ab5b879f7b9fa7bde570
Refilwe-M-Mashile <refilwe-mashile.rm@gmail.com>
2022-04-27 19:17:02 +0200

    Added script to add Tables

---
+++ CoffeeShop/SQL/CreateTables.sql
@@ -0,0 +1,33 @@
+USE master;
+
+USE CoffeeShopDB
+GO
+	CREATE TABLE Customers(
+		CustomerID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+		FirstName VARCHAR(100) NOT NULL,
+		LastName VARCHAR(100) NOT NULL,
+		DOB DATE NOT NULL,
+		Race VARCHAR(10) NOT NULL,
+		Email VARCHAR(100) NULL,
+	);
+
+GO
+	CREATE TABLE Baristas(
+		BaristaID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+		FirstName VARCHAR(100) NOT NULL,
+		LastName VARCHAR(30) NOT NULL,
+		Rating INT CHECK (
+			Rating <= 5
+			AND Rating >= 1
+		) NOT NULL,
+	);
+
+GO
+	CREATE TABLE Orders(
+		OrderID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+		OrderedBy INT NOT NULL FOREIGN KEY REFERENCES Customers(CustomerID),
+		CoffeeName VARCHAR(30) NOT NULL,
+		Quantity INT NOT NULL,
+		CoffeePrice SMALLMONEY NOT NULL,
+		OrderAssignee INT NOT NULL FOREIGN KEY REFERENCES Baristas(BaristaID),
+	);
